c2b79430c1213ed1db0c12c717310c791480202f
ZV <positive.engine@gmail.com>
2014-06-20 17:58:46 +0300

    exam program
    interface + reading tags

---
+++ XMLEditor/Classes/File.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLEditor.Classes
+{
+   public class File
+    {
+      
+       private string fileAddress;
+       public HashSet<string> tags = new HashSet<string>();      
+       public void setFileTags()
+       {
+           XmlTextReader reader = null;             
+           try
+           {                
+               // Load the reader with the XML file.
+               reader = new XmlTextReader(fileAddress);
+               // Parse the file.  If they exist, display the prefix and 
+               // namespace URI of each node.
+               while (reader.Read())
+               {
+                   if (reader.IsStartElement())
+                   {
+                       if (reader.Prefix == String.Empty)
+                           tags.Add(reader.LocalName.ToString());
+                   }
+               }
+           }
+           finally
+           {
+               if (reader != null)
+                   reader.Close();
+           }           
+       }
+       public File(string address) { fileAddress = address; setFileTags(); tags.Distinct(); }
+    }
+}
---
+++ XMLEditor/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using XMLEditor.Classes;
+using System.Windows.Forms;
+
+namespace XMLEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void updateListBox()
+        {   
+            if (ToAllRadio.IsChecked == true) 
+            {
+                HashSet<string> s = new HashSet<string>();
+               // s.Intersect()
+            }
+        }
+       //.. public List<string> ListOfFiles()
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+           /* File F=new File();
+            List<string> s = F.getFileTags();*/
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.ShowDialog();
+            if (OFD.FileName != null) 
+            {
+                File f = new File(OFD.FileName);
+                foreach (string tag in f.tags) { System.Console.WriteLine(tag);}//перевірка чи пахає запис в множину
+            }
+
+            
+        }
+    }
+}
